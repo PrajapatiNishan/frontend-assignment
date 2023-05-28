b3b6ef463290c6637b034220772455535da2bbcc
NishanPrajapati <119466701+PrajapatiNishan@users.noreply.github.com>
2023-05-28 12:41:39 +0545

    deleted the created music app due to some issues

---
+++ music-app/.next/server/flight-server-css-manifest.js
@@ -0,0 +1,14 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/nishanprajapati/Desktop/appli/frontend-assignment/music-app/src/app/layout.tsx": [
+      "/Users/nishanprajapati/Desktop/appli/frontend-assignment/music-app/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/Users/nishanprajapati/Desktop/appli/frontend-assignment/music-app/src/app/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/Users/nishanprajapati/Desktop/appli/frontend-assignment/music-app/src/app/page": [
+      "/Users/nishanprajapati/Desktop/appli/frontend-assignment/music-app/src/app/globals.css",
+      "/Users/nishanprajapati/Desktop/appli/frontend-assignment/music-app/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}
